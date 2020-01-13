6e84673ca26f3a81359b2b352fe560b92dece23f
David B <djbrook4@ncsu.edu>
2020-01-13 16:12:21 -0500

    Removed Camera Server

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -8,7 +8,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import  edu.wpi.first.cameraserver.CameraServer;
 
 public class ExampleSubsystem extends SubsystemBase {
   /**
